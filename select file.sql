c005df2eef81e5c99abee332468dd0881c4efe5b
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-06-02 22:31:13 +0530

    Add files via upload

---
+++ select file.sql
@@ -0,0 +1,10 @@
+select *
+from parks_and_recreation.employee_demographics;
+
+select first_name,last_name,age
+from parks_and_recreation.employee_demographics;
+
+select first_name,
+last_name,
+age+10
+from parks_and_recreation.employee_demographics;
