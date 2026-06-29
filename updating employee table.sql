e22c5bf977f88666e7ebc392477614eaf0d9f57f
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-06-29 23:50:09 +0530

    Set hourly pay for HR employees to 150
    Update hourly pay for HR employees to 150 and select all records.

---
+++ updating employee table.sql
@@ -0,0 +1,10 @@
+/* Write a query to do the following
+- Set hourly_pay to 150 for HR employees
+- Output the entire table
+*/
+update employee
+set Hourly_pay = 150
+where Department = 'Hr';
+select *
+from employee;
+--Updating data when the dept is hr then then hourly pay is 150 
