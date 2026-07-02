731711e324a39b98682cdc2b092d18e11da75202
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-07-02 18:54:38 +0530

    Add comment for BETWEEN clause usage
    Added a comment explaining the use of the BETWEEN clause.

---
+++ Between clause.sql
@@ -0,0 +1,6 @@
+/* Write a query to do the following
+- Output all entries from the table
+- Where passenger_name between 'A' and 'D' */
+select * from flights
+where Passenger_name between 'A' and 'D';
+--between used when a letter or num need to be checked within a range 
