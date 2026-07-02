8303ecd9efdbde4533de780aae330ae090cb03c9
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-07-02 19:07:30 +0530

    Create Like clause.sql

---
+++ Like clause.sql
@@ -0,0 +1,5 @@
+/* Write a query that returns the rows which meet the following conditions
+- Passenger names that end with 'a' */
+select * from flights 
+where Passenger_name like '%__a%';
+--like is similar type of data in the given row
